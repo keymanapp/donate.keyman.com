0f9488e71a696d7dadfa925a93cb1d1c8f91e9df
SeriousMatt <mattrzr@gmail.com>
2019-03-11 23:02:25 -0400

    Remove php code, add routes, begin error handling

--- stripe-node/app.js
+++ stripe-node/app.js
@@ -1,42 +1,39 @@
 // API with basic form
 // Set local environment variables
-const keyPublishable = 'pk_test';
-const keySecret = 'sk_test';
+const keyPublishable = 'pk_test_raF8g0kIcx1D8FlIAyxYMRed';
+const keySecret = 'sk_test_xd0rGDIVROhzmczhFF3avj2O';
 // Set up express
 const app = require("express")();
+const routes = require('./routes/routes');
 const stripe = require("stripe")(keySecret);
-// Todo: Clean up pug form, replace with angular
-app.set("view engine", "pug");
-var bodyParser = require('body-parser')
-// Send header to allow post request
+const bodyParser = require('body-parser');
+// Set routes
+app.use('/', routes);
+// Parse requests
 app.use(bodyParser.json());
+// Allow requests
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-// Home
-app.get("/", (req, res) =>
-  res.render("index.pug", {keyPublishable}));
-app.post("/charge", (req, res) => {
-  let amount = 500;
-
-  stripe.customers.create({
-     email: req.body.stripeEmail,
-    source: req.body.stripeToken
-  })
-  .then(customer =>
-    stripe.charges.create({
-      amount,
-      description: "Sample Charge",
-         currency: "usd",
-         customer: customer.id
-    }))
-  .then(charge => res.render("charge.pug"));
-});
+// // Todo: Error handling
+// app.use( function(req, res, next) {
+//     const err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+// // // Display error
+// app.use( function(err, req, res, next) {
+//     res.status( err.code || 500 )
+//     .json({
+//         status: 'error',
+//         message: err,
+//     });
+// });
 // Set up post endpoint for angular form
 app.post("/charge/angular", (req, res) => {
-  console.log (req.body);
+  // Output event
   res.send('Saw that POST!');
 // Create customer first
   stripe.customers.create({
@@ -48,11 +45,28 @@ app.post("/charge/angular", (req, res) => {
     stripe.charges.create({
       amount: req.body.amount,
       currency: req.body.currency,
-      description: 'Example charge',
-      customer: customer.id
+      description: 'Donation to Keyman',
+      customer: customer.id,
+      receipt_email: req.body.token.email
     }))
-//Todo: Set up success/fail message
-   // .then(charge => res.render("charge.pug"));
+// Successfull api call
+  // .then(res => {
+  //   res.status(200)
+  //     .json({
+  //       status: '200',
+  //       message: res,
+  //     });
+  // })
+ // Handle api failure
+  // .catch(res => {
+  //   res.status(500)
+  //      .json({
+  //        status: '500',
+  //        message: res,
+  //      });
+  // })
 });
-// Listen on Port
-app.listen(4567);
+// Listen on port 4567
+app.listen(process.env.PORT || 4567, function() {
+  console.log('Running on port 4567');
+});
