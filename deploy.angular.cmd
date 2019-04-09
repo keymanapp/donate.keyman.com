7856e97dfd1036b824a621f59a4c7c169c4625ae
Matthew Reynolds <mattrzr@gmail.com>
2019-04-09 17:49:32 -0400

    test

--- deploy.angular.cmd
+++ deploy.angular.cmd
@@ -115,15 +115,15 @@ call :ExecuteCmd !NPM_CMD! run build
 IF !ERRORLEVEL! NEQ 0 goto error
 popd
 )
-:: 4. KuduSync
+echo 4. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
-:: 5. Select node version
+echo 5. Select node version
 call :SelectNodeVersion
 
-:: 6. Install npm packages
+echo 6. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\stripe-node\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install --production
