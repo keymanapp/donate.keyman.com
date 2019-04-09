3ecb4c8cc153f59536b39dbb251cc7bc6d19aa9f
Matthew Reynolds <matthewreynolds@matthews-mbp.home>
2019-04-09 12:05:36 -0400

    Work with just stripe-api

--- deploy.cmd
+++ deploy.cmd
@@ -92,39 +92,15 @@ echo Handling node.js deployment.
 echo 1. Select node version
 call :SelectNodeVersion
 
-echo  2. Install npm packages
-IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
-  pushd "%DEPLOYMENT_SOURCE%"
-  call :ExecuteCmd !NPM_CMD! install
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
-echo 3. Angular Prod Build
-IF EXIST "%DEPLOYMENT_SOURCE%/angular.json" (
-echo Building App in %DEPLOYMENT_SOURCE%…
-pushd "%DEPLOYMENT_SOURCE%"
-cd ./node_modules/@angular/cli
-rd /s /q node_modules
-call !NPM_CMD! install
-cd D:\home\site\repository
-rd /s /q node_modules
-call !NPM_CMD! install
-call :ExecuteCmd !NPM_CMD! run build
-:: If the above command fails comment above and uncomment below one
-:: call ./node_modules/.bin/ng build –prod
-IF !ERRORLEVEL! NEQ 0 goto error
-popd
-)
 echo 4. Stripe-api build
 cd D:\home\site\repository\stripe-node
 call !NPM_CMD! install
-call :ExecuteCmd !NPM_CMD! start
 cd D:\home\site\repository
 IF !ERRORLEVEL! NEQ 0 goto error
 popd
 :: 4. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/stripe-node" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
