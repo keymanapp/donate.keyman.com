7494348cf8da46b2f193c27963339086deb34321
Matthew Reynolds <mattrzr@gmail.com>
2019-04-16 17:06:08 -0400

    Set up basic page test and begin donation test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,31 @@
 import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
 
-describe('Hannah App', () => {
+describe('Donate Keyman', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Verify page is up with text', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getTitle()).toEqual('Donate now');
+  });
+
+  it('Make a donation', () => {
+    page.navigateTo();
+    var donateTwenty = element(by.className('btn-lg btn-secondary amount-button amount-control'));
+    var donate = element(by.className('col cc-button'));
+    // Select twenty to donate
+    donateTwenty.click();
+    // Click donate
+    donate.click();
+    //Todo: Find input for email, send test email
+    var email = element(by.model('email'));
+    email.sendKeys('test@test.com');
+    // paymentInfo.click();
+
+    expect(email.isPresent());
   });
 });
